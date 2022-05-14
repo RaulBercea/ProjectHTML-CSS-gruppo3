48fce647604109301f3dfa23cd79d8c55c4bb7d2
Raul Bercea <raulbercea@gmail.com>
2022-05-14 15:28:09 +0200

    sus

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,11 @@
-function displayMenu() {
-	let checkbox = document.getElementById("menuCheck");
-	let navbar = document.getElementById("navbar");
+// getting the hamburger menu in the html file
+let hamburgerMenu = document.getElementById("hamburger");
 
-	if (checkbox.checked == true) {
-		navbar.style.display = "block";
-	} else {
-		navbar.style.display = "none";
-	}
-}
+// getting the navbar in the html file
+let navbar = document.getElementById("navbar");
+
+// event listener for the click of the hamburger menu that will display the nabbar
+hamburgerMenu.addEventListener("click", () => {
+	// with classList.toggle i can enable and disable a class from an element
+	navbar.classList.toggle("visibility");
+});
